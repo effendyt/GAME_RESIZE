900d7790420c248881e3c8e90aa25e3dcee5c99e
Effendy Tham <effendy.thegalaxy@gmail.com>
2015-11-15 09:43:29 +0800

    Script Initialization
    Added Script for
    1. Controller Engine : for detection input control and handle what
    should be done
    2. Auto Engine : for running auto process services
    3. Playable Character Engine : handle basic playable character work like
    moving and jumping
    4. Player Scientist : handle the game object scientist
    5. Player Rival Scientist : handle the game object rival scientist

---
+++ Resize/Assets/_script/playableCharacterEngine.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class playableCharacterEngine : MonoBehaviour {
+
+
+	public double speedMoving;
+	public double speedJumping;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
